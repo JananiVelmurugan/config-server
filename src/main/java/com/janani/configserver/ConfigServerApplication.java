6f1d891095d5a7826d7a17765d1ecadd18a3071b
Janani <jananise@gmail.com>
2017-10-05 17:18:35 +0530

    config server prop

--- src/main/java/com/janani/configserver/ConfigServerApplication.java
+++ src/main/java/com/janani/configserver/ConfigServerApplication.java
@@ -4,25 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String index() {
+
+		System.out.println("index");
+		return "hi";
+
+	}
 }
 
-//@Configuration
-//@EnableAutoConfiguration
-//@EnableDiscoveryClient
-//@EnableConfigServer
-//public class ConfigServerApplication {
+// @Configuration
+// @EnableAutoConfiguration
+// @EnableDiscoveryClient
+// @EnableConfigServer
+// public class ConfigServerApplication {
 //
-//	public static void main(String[] args) {
-//		SpringApplication.run(ConfigServerApplication.class, args);
-//	}
+// public static void main(String[] args) {
+// SpringApplication.run(ConfigServerApplication.class, args);
+// }
 //
-//}
+// }
